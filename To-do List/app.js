1e1563e3be0782e0aee71b9e036fd38754922e15
Elijah Ohiwerei <elijahohimairei@gmail.com>
2021-04-25 18:54:34 +0100

    files

--- To-do List/app.js
+++ To-do List/app.js
@@ -1,9 +1,30 @@
-let welcome = prompt("What do you want to do");
-let input = "new";
-while (true) {
-    if (welcome === input) {
-        welcome = prompt("Enter new todo");
-        let welcome = new welcome;
-        console.log(welcome)
+let input = prompt("What do you want to do");
+const todos = ['Collect Chicken Eggs', 'Be Happy']
+
+
+
+while (input !== 'quit' && input !== 'q') {
+    if (input === 'list') {
+        console.log('sneh')
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+    } else if (input === 'new') {
+        const newTodo = prompt('Ok, what is the new todo?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list`);
     }
+    else if (input === 'delete') {
+        const index = parseInt(prompt('Ok enter an index to delete'));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+        } else {
+            console.log('Unknown Index')
+        }
+
+
+    }
+    input = prompt("What do you want to do");
 }
+console.log("You Quit");
