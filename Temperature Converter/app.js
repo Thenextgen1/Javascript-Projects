3325436ae4c0fd5530ea949eec26abae672dec6c
Elijah Ohiwerei <elijahohimairei@gmail.com>
2021-05-16 23:22:25 +0100

    files

--- Temperature Converter/app.js
+++ Temperature Converter/app.js
@@ -12,16 +12,29 @@ formTemp.addEventListener('submit', (e) => {
     const FahrenheitInput = formTemp.elements.fahTemp;
     // let celsiusNum = parseInt(celsiusInput.value);
     // let fahrenheitNum = parseInt(FahrenheitInput.value)
-    calculate(parseInt(celsiusInput.value), parseInt(FahrenheitInput.value));
-    celsiusInput.value = '';
-    FahrenheitInput.value = '';
+
+    if (celsiusInput.value && typeof parseInt(celsiusInput.value === 'number')) {
+
+        FahrenheitInput.value = calculate(parseInt(celsiusInput.value));
+        celsiusInput.value = '';
+    }
+    else if (FahrenheitInput.value && typeof parseInt(FahrenheitInput.value === 'number')) {
+
+        celsiusInput.value = calculate(undefined, parseInt(FahrenheitInput.value));
+        FahrenheitInput.value = '';
+    }
+    else if (typeof celsiusInput.value === 'string' || typeof FahrenheitInput.value === 'number') {
+        FahrenheitInput.value = '';
+        celsiusInput.value = '';
+    }
+
 })
 const calculate = (num1, num2) => {
     if (num1) {
-        console.log(num1 * (9 / 5) + 32)
+        return num1 * (9 / 5) + 32;
     }
     else if (num2) {
-        console.log((num2 - 32) * 5 / 9)
+        return (num2 - 32) * 5 / 9;
     }
 
 }
